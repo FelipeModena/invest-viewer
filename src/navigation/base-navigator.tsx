5f7f1a88732b9f6b1acccc2be33b3ad9670a3b3e
Modena <felipe.kroitor@gmail.com>
2023-10-11 16:05:29 -0300

    feat: extension changed for ts

--- src/navigation/base-navigator.tsx
+++ src/navigation/base-navigator.tsx
@@ -9,6 +9,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
 export function BaseNavigator() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
@@ -21,13 +25,13 @@ function HomeStackNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen
+        name="ContractDetailScreen"
+        component={ContractDetailScreen}
+      />
+      <Stack.Screen
         options={{ headerShown: false }}
         name="HomeScreen"
         component={Home}
-      />
-      <Stack.Screen
-        name="ContractDetailScreen"
-        component={ContractDetailScreen}
       />
     </Stack.Navigator>
   );
