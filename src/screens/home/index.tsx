7f31fe775f2b2c4b0edc6cfaf8ab0fb45b2e891f
Modena <felipe.kroitor@gmail.com>
2023-10-14 15:00:54 -0300

    Update index.tsx

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,6 @@
 //give me a generic home page
 
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -9,59 +9,48 @@ import {
   TextInput,
   ScrollView,
   SafeAreaView,
+  SafeAreaViewBase,
 } from "react-native";
 import { genericStyles } from "../../assets/styles/style";
 import { ContractChart } from "../../components/home/contracts-chart";
-import InfoCard from "../../components/generic/info-card";
+import InfoCard from "../../components/home/info-card";
+import contracts from "../../assets/mocks/contracts";
+import contractsMock from "../../assets/mocks/contracts";
+import axiosOpenExInstance from "../../services/axios";
 
-const contractsList = [
-  {
-    id: 1,
-    contractNumber: "000457",
-    actualRoi: 7.3,
-    contractInitialValue: 20000,
-    contractActualValue: 22304,
-  },
-  {
-    id: 2,
-    contractNumber: "000458",
-    actualRoi: 8.1,
-    contractInitialValue: 25000,
-    contractActualValue: 27025,
-  },
-
-  {
-    id: 4,
-    contractNumber: "000460",
-    actualRoi: 9.21,
-    contractInitialValue: 30000,
-    contractActualValue: 32760,
-  },
-];
-
-const infoCardsContracts = contractsList.map((contract) => {
+const infoCardsContracts = contractsMock.map((contract) => {
   return (
     <InfoCard
       actualRoi={contract.actualRoi}
-      key={contract.id}
-      contractActualValue={contract.contractActualValue}
-      contractInitialValue={contract.contractInitialValue}
-      contractNumber={contract.contractNumber}
-      id={contract.id}
+      key={contract.contract}
+      contractActualValue={contract.actualContractValue}
+      contractInitialValue={contract.initialInvestment}
+      contractNumber={contract.contract}
+      id={parseInt(contract.contract)}
       routeTo={"detail"}
     />
   );
 });
 
-export function Home() {
+export function HomeScreen() {
+  const [rates, setRates] = useState("");
+
+  async function getRates() {
+    const res = await axiosOpenExInstance.get("latest.json/");
+    console.log(res.data);
+    setRates(res.data.rates.UAH);
+  }
+
   return (
-    <ScrollView style={genericStyles.body}>
-      <ContractChart />
-      <View style={genericStyles.container}>
-        <Text>Contratos ativos</Text>
-        {infoCardsContracts}
-      </View>
-    </ScrollView>
+    <SafeAreaView>
+      <ScrollView style={genericStyles.body}>
+        <ContractChart />
+        <View style={genericStyles.container}>
+          <Text>Contratos ativos</Text>
+          {infoCardsContracts}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
