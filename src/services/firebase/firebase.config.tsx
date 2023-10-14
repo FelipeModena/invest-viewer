77280a79c1d72881bec27a1b83b0d50036ec85c9
Modena <felipe.kroitor@gmail.com>
2023-10-14 15:20:05 -0300

    Update firebase.config.tsx

--- src/services/firebase/firebase.config.tsx
+++ src/services/firebase/firebase.config.tsx
@@ -8,16 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-console.log("firebase.config.tsx");
-
 const firebaseConfig = {
-  apiKey: process.env.EXPO_APIKEY,
-  authDomain:  process.env.EXPO_AUTHDOMAIN,
-  projectId:  process.env.EXPO_PROJECTID,
-  storageBucket:  process.env.EXPO_STORAGEBUCKET,
-  messagingSenderId:  process.env.EXPO_MESSAGINGSENDERID,
-  appId:  process.env.EXPO_APPID,
-  measurementId:  process.env.EXPO_MEASUREMENTID,
+  apiKey: process.env.EXPO_PUBLIC_APIKEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.EXPO_PUBLIC_APPID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
 };
 
 // Initialize Firebase
@@ -25,4 +23,4 @@ const FIREBASE_APP = initializeApp(firebaseConfig);
 const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 // const firebaseAnalytics = getAnalytics(firebaseApp);
 
-export { FIREBASE_APP };
+export { FIREBASE_APP, FIREBASE_AUTH };
