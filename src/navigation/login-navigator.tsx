9dd51e9d8e95a56138c97babef80bae8ad6a425d
Modena <felipe.kroitor@gmail.com>
2023-10-17 13:36:52 -0300

    feat: auth for login and base route created

--- src/navigation/login-navigator.tsx
+++ src/navigation/login-navigator.tsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Login } from "../screens/login";
 import { RootNavigator } from "./base-navigator";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { FIREBASE_AUTH } from "../services/firebase/firebase.config";
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export function LoginNavigator() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="LoginScreen" component={Login} />
-      <Stack.Screen name="Base" component={RootNavigator} />
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="LoginScreen"
+    >
+      {user ? (
+        <Stack.Screen name="Base" component={RootNavigator} />
+      ) : (
+        <Stack.Screen name="LoginScreen" component={Login} />
+      )}
     </Stack.Navigator>
   );
 }
