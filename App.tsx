3f0d97c99377a3f8d646d4ea6195356392384a41
Modena <felipe.kroitor@gmail.com>
2023-10-17 13:38:50 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import React from "react";
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <BaseNavigator /> */}
       <LoginNavigator />
     </NavigationContainer>
   );
